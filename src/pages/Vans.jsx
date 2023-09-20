7674f5e6e35a543b40c5060666d4e98e595ceb86
anirudh428 <annabathula5084@mydusd.org>
2023-09-20 16:19:38 -0700

    Update Vans.jsx

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 export default function Vans() {
+    const [vans, setVans] = React.useState([])
+    React.useEffect(() => {
+        fetch("/api/vans").then(res => res.json()).then(data => setVans(data.vans))
+        
+    }, [])
+    console.log(vans)
+    const vanElements = vans.map(van => {
+        <div key={van.id} className="van-tile">
+            <img src={van.imageUrl} />
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}<span>/day</span></p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </div>
+    })
   return (
-    <div>
-        <h1>Vans page goes here</h1>
-    </div>
+    <>
+        {vanElements}
+    </>
   )
 }
