df104888e5f2223073cb8aa57eef342437a5a58f
anirudh428 <annabathula5084@mydusd.org>
2023-09-20 23:51:11 -0700

    Update Vans.jsx

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -8,14 +8,14 @@ export default function Vans() {
     }, [])
     console.log(vans)
     const vanElements = vans.map(van => {
-        <div key={van.id} className="van-tile">
+        return (<div key={van.id} className="van-tile">
             <img src={van.imageUrl} />
             <div className="van-info">
                 <h3>{van.name}</h3>
                 <p>${van.price}<span>/day</span></p>
             </div>
             <i className={`van-type ${van.type} selected`}>{van.type}</i>
-        </div>
+        </div>)
     })
   return (
     <>
